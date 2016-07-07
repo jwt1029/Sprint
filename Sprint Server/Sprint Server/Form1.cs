dc963f4c56c19ec41a4a6606110fb2d5f25ff1e2
XenoAron <xenoaron@creative-developer.com>
2016-07-07 20:11:48 +0900

    Initialize Server
    Initialize Server

---
+++ Sprint Server/Sprint Server/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sprint_Server
+{
+    public partial class Form1 : Form
+    {
+        private const string logHeader = "[LOG]";
+        private Socket serverSocket = null;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
